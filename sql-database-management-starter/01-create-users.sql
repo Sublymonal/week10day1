31459993f9c37f58f9f1b1c6a55b44d2f68e4974
Ace <33190163+Sublymonal@users.noreply.github.com>
2021-06-08 05:14:10 -0700

    Update 01-create-users.sql
    specified naming convention and password case sensitivity

--- sql-database-management-starter/01-create-users.sql
+++ sql-database-management-starter/01-create-users.sql
@@ -18,8 +18,8 @@ create USER data_admin with password 'ik2fiDri' superuser;
 
 -- when creating a user , use the following syntax
 --
--- create user (>>USER NAME<<) with password (>>'password'<<);
+-- create user (>>'USER_NAME'<<) with password (>>'p@5sW0rd'<<);
 --
 -- if creating a super user, add superuser at the end of new user creation
 
--- create user (>>USER NAME<<) with password (>>'password'<<) superuser;
+-- create user (>>'USER_NAME'<<) with password (>>'p@5sW0rd'<<) superuser;
